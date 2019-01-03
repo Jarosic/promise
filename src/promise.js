80328793dd135b041a12b2f1ebce9819778149af
Jarosic <jaroslav.rybalka@ukr.net>
2019-01-03 13:23:44 +0200

    more arguments added to constructor

--- src/promise.js
+++ src/promise.js
@@ -3,6 +3,14 @@ class OwnPromise {
     if (typeof executor !== 'function') {
       throw new Error('executor is not a function');
     }
+
+    this.state = 'PENDING';
+    this.value = undefined;
+    this.thenners = [];
+    this.resolve = this.resolve.bind(this);
+    this.reject = this.reject.bind(this);
+
+    executor(this.resolve, this.reject);
   }
 }
 
