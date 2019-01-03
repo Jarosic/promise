dc0c75bbf99c9d3b526d4a480463e9ac6440316f
Jarosic <jaroslav.rybalka@ukr.net>
2019-01-03 12:50:25 +0200

    constructor has been created

--- src/promise.js
+++ src/promise.js
@@ -1,5 +1,9 @@
 class OwnPromise {
-
+  constructor(executor) {
+    if (typeof executor !== 'function') {
+      throw new Error('executor is not a function');
+    }
+  }
 }
 
 module.exports = OwnPromise;
